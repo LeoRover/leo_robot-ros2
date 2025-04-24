282e2150e96e56ed63e002e766c1e0fc4eff19e9
Aleksander Szymański <bitterisland6@gmail.com>
2025-04-24 14:37:40 +0200

    leo_filters: odom_filter: remove usage of depreacted rmw struct
    Signed-off-by: Aleksander Szymański <bitterisland6@gmail.com>

--- leo_filters/src/odom_filter.cpp
+++ leo_filters/src/odom_filter.cpp
@@ -49,7 +49,7 @@ OdomFilter::OdomFilter(rclcpp::NodeOptions options)
   client_cb_group_ =
     create_callback_group(rclcpp::CallbackGroupType::MutuallyExclusive);
   reset_odom_client_ = create_client<std_srvs::srv::Trigger>(
-    "firmware/reset_odometry", rmw_qos_profile_services_default,
+    "firmware/reset_odometry", rclcpp::ServicesQoS(),
     client_cb_group_);
   reset_odom_srv_ = create_service<std_srvs::srv::Trigger>(
     "reset_odometry",
