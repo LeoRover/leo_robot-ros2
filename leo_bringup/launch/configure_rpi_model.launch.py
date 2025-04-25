bc031087aeaa67e76109b8173f32e78b41f7ddd7
Jan Hernas <hernasj@wp.pl>
2025-04-25 15:36:04 +0200

    Revert camera to xml launch file

---
+++ leo_bringup/launch/configure_rpi_model.launch.py
@@ -0,0 +1,22 @@
+from launch import LaunchDescription
+from launch.actions import SetLaunchConfiguration
+
+
+def generate_launch_description():
+    rpi_model = -1
+    
+    try:
+        with open("/proc/device-tree/model", "r", encoding="utf-8") as f:
+            model = f.read().strip()
+        if "Raspberry Pi 5" in model:
+            rpi_model = 5
+        elif "Raspberry Pi 4" in model:
+            rpi_model = 4
+    except (FileNotFoundError, IOError) as e:
+        print(f"Failed to read Raspberry Pi model: {e}")
+    
+    return LaunchDescription([
+        SetLaunchConfiguration(name='rpi_model', 
+                               value=str(rpi_model)),
+    ])
+
