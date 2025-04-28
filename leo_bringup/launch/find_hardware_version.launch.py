b57a097b33c754cb93056b2b1be3637129bb45e1
Błażej Sowa <bsowa123@gmail.com>
2025-04-28 10:30:07 +0000

    Refactor hardware version detection and update camera configuration for Leo Rover

--- leo_bringup/launch/find_hardware_version.launch.py
+++ leo_bringup/launch/find_hardware_version.launch.py
@@ -3,15 +3,15 @@ from launch.actions import SetLaunchConfiguration
 
 
 def generate_launch_description():
-    leo_hardware_version = 109
+    leo_hardware_version = 1
 
     try:
         with open("/proc/device-tree/model", "r", encoding="utf-8") as f:
             model = f.read().strip()
         if "Raspberry Pi 5" in model:
-            leo_hardware_version = 109
+            leo_hardware_version = 2
         elif "Raspberry Pi 4" in model:
-            leo_hardware_version = 108
+            leo_hardware_version = 1
     except (FileNotFoundError, IOError) as e:
         print(f"Failed to read Raspberry Pi model: {e}")
 
