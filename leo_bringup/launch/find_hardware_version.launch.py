c1de264d8acb70458b652cc81bd2220988d8edf2
Błażej Sowa <bsowa123@gmail.com>
2025-09-12 17:08:01 +0200

    Fix pylint errors

--- leo_bringup/launch/find_hardware_version.launch.py
+++ leo_bringup/launch/find_hardware_version.launch.py
@@ -6,14 +6,14 @@ def generate_launch_description():
     leo_hardware_version = 1
 
     try:
-        with open("/proc/device-tree/model", "r", encoding="utf-8") as f:
-            model = f.read().strip()
+        with open("/proc/device-tree/model", "r", encoding="utf-8") as file:
+            model = file.read().strip()
         if "Raspberry Pi 5" in model:
             leo_hardware_version = 2
         elif "Raspberry Pi 4" in model:
             leo_hardware_version = 1
-    except (FileNotFoundError, IOError) as e:
-        print(f"Failed to read Raspberry Pi model: {e}")
+    except (FileNotFoundError, IOError) as err:
+        print(f"Failed to read Raspberry Pi model: {err}")
 
     return LaunchDescription(
         [
